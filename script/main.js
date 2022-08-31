038f2fd74e361672cd91dbc6aa73e59709cdbea3
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-08-31 16:41:57 +0200

    add Groups And footer

--- script/main.js
+++ script/main.js
@@ -22,3 +22,38 @@ if(overlay){
     })
 }
 
+// Fetch Json Api
+function getGroups(){
+  return new Promise((res)=>{
+    let url ='script/group.json';
+    fetch(url).then((response)=>{
+      res(response.json());
+    });
+  });
+}
+getGroups();
+async function displayData(){
+  let Groups = document.querySelector('.groups .container');
+  try{
+    let card =``;
+    const data = await getGroups();
+    data.forEach((d)=>{
+      card+=`
+        <div class="box">
+          <div class="img">
+              <img src="${d.img}" alt="${d.title}-group">
+          </div>
+          <div class="text">
+            <a href="#"><h3>${d.title}</h3></a>
+              <p>${d.description}</p>
+          </div>
+              <a class = "readMap"href="#">Read Map</a>
+        </div>
+      `;
+    });
+    Groups.innerHTML = card;
+  } catch (e) {
+    console.log(e);
+  }
+  }
+  displayData();
